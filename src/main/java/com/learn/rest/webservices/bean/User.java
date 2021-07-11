c22f35549f8509fd8e9c3512e0959209dce34c5e
akhi <akhi@Akhilesh>
2021-07-11 13:11:17 +0530

    Add validation

--- src/main/java/com/learn/rest/webservices/bean/User.java
+++ src/main/java/com/learn/rest/webservices/bean/User.java
@@ -2,11 +2,16 @@ package com.learn.rest.webservices.bean;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 	private Integer id;
 
+	@Size(min=2, message="Name should have atleast 2 characters")
 	private String name;
-
+	
+	@Past
 	private Date birthDate;
 
 	protected User() {
