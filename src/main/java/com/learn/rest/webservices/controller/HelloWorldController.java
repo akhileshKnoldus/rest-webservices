4708e89d11584641e26531a7acdedf62ce67cf75
akhi <akhi@Akhilesh>
2021-07-11 12:32:48 +0530

    CRUD Operation

---
+++ src/main/java/com/learn/rest/webservices/controller/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.learn.rest.webservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.rest.webservices.bean.HelloWorldBean;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	///hello-world/path-variable/akhilesh
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+}
